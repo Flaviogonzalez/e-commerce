813e2660c0eb66d1e6f2b0343438d414427d6b73
Flavio Gonzalez <84059951+Flaviogonzalez@users.noreply.github.com>
2025-12-02 12:18:52 -0300

    Refactor authentication request and response structures; remove old definitions and add new ones

--- auth/go.mod
+++ auth/go.mod
@@ -4,6 +4,8 @@ go 1.25.2
 
 require github.com/jackc/pgx/v5 v5.7.6
 
+require github.com/flaviogonzalez/e-commerce/contracts v0.0.0-20251202143131-3dc19213430c // indirect
+
 require (
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/go-chi/cors v1.2.2
---
+++ contracts/contracts.go
@@ -0,0 +1,11 @@
+package contracts
+
+type AuthRegisterRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthRegisterResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
